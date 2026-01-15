892c90124a6e0f4d73066a3d93ff6703a3733cfa
Markus Färber <mf@softvise.de>
2026-01-15 08:32:12 +0100

    [frontend] Make SelectCountry control work

--- frontend/src/components/SelectCountry.tsx
+++ frontend/src/components/SelectCountry.tsx
@@ -1,84 +1,65 @@
-import React from 'react';
+import React from "react";
 
-import Box              from '@mui/material/Box';
-import Checkbox         from '@mui/material/Checkbox';
-import CircularProgress from '@mui/material/CircularProgress';
-import FormControl      from '@mui/material/FormControl';
-import IconButton       from '@mui/material/IconButton';
-import InputLabel       from '@mui/material/InputLabel';
-import List             from '@mui/material/List';
-import ListItem         from '@mui/material/ListItem';
-import ListItemText     from '@mui/material/ListItemText';
-import Menu             from '@mui/material/Menu';
-import MenuItem         from '@mui/material/MenuItem';
-import Select           from '@mui/material/Select';
+import Box              from "@mui/material/Box";
+import Checkbox         from "@mui/material/Checkbox";
+import CircularProgress from "@mui/material/CircularProgress";
+import FormControl      from "@mui/material/FormControl";
+import Divider          from "@mui/material/Divider";
+import IconButton       from "@mui/material/IconButton";
+import InputLabel       from "@mui/material/InputLabel";
+import List             from "@mui/material/List";
+import ListItem         from "@mui/material/ListItem";
+import ListItemText     from "@mui/material/ListItemText";
+import Menu             from "@mui/material/Menu";
+import MenuItem         from "@mui/material/MenuItem";
+import Select           from "@mui/material/Select";
+import Stack            from "@mui/material/Stack";
 
-import FilterListIcon from '@mui/icons-material/FilterList';
+import FilterListIcon from "@mui/icons-material/FilterList";
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-type Option = {
-    id: number;
-    name: string;
-};
+import useQuery from "../hooks/useQuery";
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-const SelectCountry: React.FC = () => {
-    const [options, setOptions] = React.useState<Option[]>([]);
-    const [loading, setLoading] = React.useState<boolean>(true);
-    const [selectedOption, setSelectedOption] = React.useState<number | ''>('');
+const SelectCountry: React.FC = ({ geonameId, handleChangeGeonameId }) => {
+    const [selectedGeonameId, setSelectedGeonameId] = React.useState<number | null>(geonameId);
 
-    // States for the config menu and checkboxes
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [selectedConfigs, setSelectedConfigs] = React.useState<string[]>([]); // To store selected checkboxes
-
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    // Construct the API URL based on selected configurations
-    const buildApiUrl = () => {
-        let baseUrl = 'https://api.example.com/options?';
-        selectedConfigs.forEach(
-            (config, index) => {
-                baseUrl += `${config}=true${index < selectedConfigs.length - 1 ? '&' : ''}`;
-            }
-        );
-        return baseUrl;
-    };
+    // States for the config menu
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+    const [continents, setContinents] = React.useState<string[]>(["EU"]);
+    const [onlyBigCountries, setOnlyBigCountries] = React.useState<boolean>(true);
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    // Fetch data from the constructed API URL
-    const fetchData = (url: string) => {
-        setLoading(true);
-        fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            setOptions(data);
-            setLoading(false);
-        })
-        .catch((error) => {
-            console.error('Error fetching data:', error);
-            setLoading(false);
-        });
-    };
+    // --- Run the SQL query when the configuration changes ---
+    const { result, run } = useQuery();
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    // Fetch data when the component mounts or when selectedConfigs change
+    // Fetch data when the component mounts or when continents change
     React.useEffect(
         () => {
-            const apiUrl = buildApiUrl();
-            fetchData(apiUrl);
+            const conditions = [];
+
+            conditions.push(`continent IN (${continents.map((cnt) => (`'${cnt}'`)).join(", ")})`);
+
+            if (onlyBigCountries)
+                conditions.push("population >= 1000000");
+
+            const sqlQuery = `SELECT geoname_id, country FROM countryInfo WHERE ${conditions.join(" AND ")} ORDER BY country ASC;`;
+
+            run(sqlQuery);
         },
-        [selectedConfigs]
+        [continents, onlyBigCountries]
     );
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     const handleChange =
         (event: React.ChangeEvent<{ value: unknown }>) => {
-            setSelectedOption(event.target.value as number);
+            const geoname_id = event.target.value as number;
+            setSelectedGeonameId(geoname_id);
+            handleChangeGeonameId(geoname_id);
         };
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -99,84 +80,108 @@ const SelectCountry: React.FC = () => {
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     // Handle checkbox toggle
-    const handleCheckboxChange =
-        (config: string) => {
-            setSelectedConfigs(
+    const handleContinentChange =
+        (code: string) => {
+            setContinents(
                 (prev) =>
-                prev.includes(config) ? prev.filter((item) => item !== config) : [...prev, config]
+                prev.includes(code) ? prev.filter((item) => item !== code) : [...prev, code]
             );
         };
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     return (
-        <div>
-            {/* Filter Icon Button */}
-            <IconButton onClick={handleClick} color="primary">
-                <FilterListIcon />
-            </IconButton>
+        <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
+            <Box>
+                {/* Filter Icon Button */}
+                <IconButton onClick={handleClick} color="primary">
+                    <FilterListIcon />
+                </IconButton>
 
-            {/* Config Menu with Checkboxes */}
-            <Menu anchorEl={anchorEl}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-            >
-                <List>
-                    <ListItem>
-                        <Checkbox checked={selectedConfigs.includes('option1')}
-                                  onChange={() => handleCheckboxChange('option1')}
-                        />
-                        <ListItemText primary="Option 1" />
-                    </ListItem>
-                    <ListItem>
-                        <Checkbox checked={selectedConfigs.includes('option2')}
-                                  onChange={() => handleCheckboxChange('option2')}
-                        />
-                        <ListItemText primary="Option 2" />
-                    </ListItem>
-                    <ListItem>
-                        <Checkbox checked={selectedConfigs.includes('option3')}
-                                  onChange={() => handleCheckboxChange('option3')}
-                        />
-                        <ListItemText primary="Option 3" />
-                    </ListItem>
-                </List>
-            </Menu>
+                {/* Config Menu with Checkboxes */}
+                <Menu anchorEl={anchorEl}
+                      open={Boolean(anchorEl)}
+                      onClose={handleClose}
+                >
+                    <List>
+                        <ListItem>
+                            <Checkbox checked={continents.includes("EU")}
+                                      onChange={() => handleContinentChange("EU")} />
+                            <ListItemText primary="Europe" />
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked={continents.includes("AS")}
+                                      onChange={() => handleContinentChange("AS")} />
+                            <ListItemText primary="Asia" />
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked={continents.includes("AF")}
+                                      onChange={() => handleContinentChange("AF")} />
+                            <ListItemText primary="Africa" />
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked={continents.includes("NA")}
+                                      onChange={() => handleContinentChange("NA")} />
+                            <ListItemText primary="North America" />
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked={continents.includes("SA")}
+                                      onChange={() => handleContinentChange("SA")} />
+                            <ListItemText primary="South America" />
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked={continents.includes("OC")}
+                                      onChange={() => handleContinentChange("OC")} />
+                            <ListItemText primary="Australia and Oceania" />
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked={continents.includes("AN")}
+                                      onChange={() => handleContinentChange("AN")} />
+                            <ListItemText primary="Antarctica" />
+                        </ListItem>
+                        <Divider />
+                        <ListItem>
+                            <Checkbox checked={onlyBigCountries}
+                                      onChange={(event) => { setOnlyBigCountries(event.target.checked); }} />
+                            <ListItemText primary="Only big countries" />
+                        </ListItem>
+                    </List>
+                </Menu>
+            </Box>
 
             {/* ComboBox */}
             <FormControl fullWidth>
-                <InputLabel>Choose an Option</InputLabel>
+                <InputLabel>Choose a Country</InputLabel>
 
-                {
-                    loading
-                        ? (
-                            <Box display="flex" justifyContent="center" alignItems="center" height="100px">
-                                <CircularProgress />
-                            </Box>
-                          )
-                        : (
-                            <Select
-                            value={selectedOption}
-                            onChange={handleChange}
-                            label="Choose an Option"
-                            >
-                                <MenuItem value="">
-                                    <em>None</em>
-                                </MenuItem>
-                                    {
-                                        options.map(
-                                            (option) => (
-                                                <MenuItem key={option.id} value={option.id}>
-                                                    {option.name}
-                                                </MenuItem>
-                                            )
+                {result.isLoading
+                    ? (
+                        <Box display="flex" justifyContent="center" alignItems="center" height="100px">
+                            <CircularProgress />
+                        </Box>
+                      )
+                    : (
+                        <Select
+                        value={selectedGeonameId}
+                        onChange={handleChange}
+                        label="Choose a Country"
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                                {
+                                    (result.data?.rows ?? []).map(
+                                        (row) => (
+                                            <MenuItem key={row.geoname_id} value={row.geoname_id}>
+                                                {row.country}
+                                            </MenuItem>
                                         )
-                                    }
-                            </Select>
-                        )
+                                    )
+                                }
+                        </Select>
+                    )
                 }
             </FormControl>
-        </div>
+        </Stack>
     );
 };
 
